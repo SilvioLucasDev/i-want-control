ce574aadea7e20f1a04884ec5eeb199105577199
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-02 16:26:38 -0300

    refactor: improve ui of table

--- resources/js/Components/Table/THead/Tr.vue
+++ resources/js/Components/Table/THead/Tr.vue
@@ -3,9 +3,10 @@ import { computed } from 'vue';
 
 const props = defineProps({
     class: String,
+    withBorder: Boolean,
 });
 
-const classes = computed(() => ['font-semibold text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-700', props.class]);
+const classes = computed(() => ['font-semibold text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-700', props.withBorder ? 'border-b border-gray-200 dark:border-gray-600' : '', props.class]);
 </script>
 
 <template>
