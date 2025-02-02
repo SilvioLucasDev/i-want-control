36eeac18cec858692787fa6def76fc0a95ee37c9
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-02 01:27:41 -0300

    feat: add Button and Icon Plus

---
+++ resources/js/Components/Buttons/PlusButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PlusIcon from '@/Components/Icons/PlusIcon.vue';
+</script>
+
+<template>
+    <button
+        class="flex items-center justify-center rounded-full border border-gray-300 bg-white shadow-md transition-all hover:scale-105 hover:bg-gray-100 hover:shadow-lg focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:hover:shadow-xl"
+    >
+        <PlusIcon />
+    </button>
+</template>
