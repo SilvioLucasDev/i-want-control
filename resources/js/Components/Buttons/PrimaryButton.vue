2fb9df5c864f76941c6b407c29ebc20536a19cd3
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-02 16:26:20 -0300

    refactor: improve ui of buttons

--- resources/js/Components/Buttons/PrimaryButton.vue
+++ resources/js/Components/Buttons/PrimaryButton.vue
@@ -1,5 +1,7 @@
 <template>
-    <button class="mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button
+        class="mb-2 me-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-300 dark:bg-white dark:text-gray-900 dark:hover:bg-gray-100 dark:focus:ring-gray-100"
+    >
         <slot />
     </button>
 </template>
--- resources/js/Components/Buttons/SecondaryButton.vue
+++ resources/js/Components/Buttons/SecondaryButton.vue
@@ -12,7 +12,7 @@ withDefaults(
 <template>
     <button
         :type="type"
-        class="dark:focus:ring-gray-7000 mb-2 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        class="dark:focus:ring-gray-7000 mb-2 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
     >
         <slot />
     </button>
