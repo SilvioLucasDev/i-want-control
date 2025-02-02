6ac6001460685e5cc91cf43f450bd1d325bfca49
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-02 16:25:47 -0300

    feat: add Avatar component

---
+++ resources/js/Components/Avatar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+    name: String,
+    image: String,
+});
+
+const initials = computed(() => {
+    if (!props.name) return 'U';
+
+    const names = props.name.split(' ');
+    return names.length > 1 ? `${names[0][0]}${names[1][0]}`.toUpperCase() : names[0][0].toUpperCase();
+});
+</script>
+
+<template>
+    <div class="flex items-center space-x-3">
+        <div class="flex h-8 w-8 items-center justify-center rounded-full bg-gray-300 font-semibold text-gray-800" v-if="!image">
+            {{ initials }}
+        </div>
+        <img v-else class="h-8 w-8 rounded-full" :src="image" alt="User avatar" />
+    </div>
+</template>
