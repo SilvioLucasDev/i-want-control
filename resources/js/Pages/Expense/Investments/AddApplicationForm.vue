2e9c138b3088ac437aec98689bed1c569fb4148e
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-02 22:40:41 -0300

    feat: add investments modal

---
+++ resources/js/Pages/Expense/Investments/AddApplicationForm.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
+import SecondaryButton from '@/Components/Buttons/SecondaryButton.vue';
+import InputError from '@/Components/Inputs/InputError.vue';
+import InputLabel from '@/Components/Inputs/InputLabel.vue';
+import TextInput from '@/Components/Inputs/TextInput.vue';
+import Modal from '@/Components/Modal/Modal.vue';
+
+import { useForm } from '@inertiajs/vue3';
+
+defineProps({
+    showApplicationModal: Boolean,
+});
+
+const emit = defineEmits(['close']);
+
+const close = (): void => {
+    emit('close');
+};
+
+const form = useForm({
+    type: '',
+    income: '',
+    value: '',
+});
+
+const save = (): void => {
+    console.log('DASHBOARD :: Save Application', form);
+};
+</script>
+
+<template>
+    <Modal :show="showApplicationModal" @close="close" max-width="md">
+        <div class="p-6">
+            <h3 class="mb-4 text-lg font-semibold text-gray-900 dark:text-white">Adicionar uma nova aplicação?</h3>
+
+            <form @submit.prevent="save" class="grid gap-4">
+                <div>
+                    <InputLabel for="type" value="Tipo" />
+                    <TextInput id="type" v-model="form.type" type="text" class="mt-1 block w-full" />
+                    <InputError class="mt-2" :message="form.errors.type" />
+                </div>
+
+                <div>
+                    <InputLabel for="income" value="Rentabilidade" />
+                    <TextInput id="income" v-model="form.income" type="text" class="mt-1 block w-full" />
+                    <InputError class="mt-2" :message="form.errors.income" />
+                </div>
+
+                <div>
+                    <InputLabel for="value" value="Valor" />
+                    <TextInput id="value" v-model="form.value" type="text" class="mt-1 block w-full" />
+                    <InputError class="mt-2" :message="form.errors.value" />
+                </div>
+
+                <div class="grid grid-cols-2 gap-2">
+                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Salvar </PrimaryButton>
+
+                    <SecondaryButton @click="close"> Cancelar </SecondaryButton>
+                </div>
+            </form>
+        </div>
+    </Modal>
+</template>
