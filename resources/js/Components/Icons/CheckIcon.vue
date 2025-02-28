5a4879b1905d60b426789263e58893d26ae52c68
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-28 00:51:05 -0300

    feat: add CheckIcon

---
+++ resources/js/Components/Icons/CheckIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+    class: String,
+    active: Boolean,
+});
+
+const classes = computed(() => ['h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white', props.active ? 'bg-gray-200 text-gray-900 dark:bg-gray-600 dark:text-white' : '', props.class]);
+</script>
+
+<template>
+    <svg :class="classes" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+    </svg>
+</template>
