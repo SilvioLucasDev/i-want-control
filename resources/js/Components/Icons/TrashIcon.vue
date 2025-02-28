242dfcbf43ef8df8c9ad272bb1fc990dfb838cbc
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-02-28 00:51:27 -0300

    feat: add TrashIcon

---
+++ resources/js/Components/Icons/TrashIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+    class: String,
+    active: Boolean,
+});
+
+const classes = computed(() => ['h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white', props.active ? 'bg-gray-200 text-gray-900 dark:bg-gray-600 dark:text-white' : '', props.class]);
+</script>
+
+<template>
+    <svg :class="classes" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+    </svg>
+</template>
