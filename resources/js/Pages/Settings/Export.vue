7eaa675cb1f02232331c25de24d0405d92743eb8
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-01 23:20:58 -0300

    feat: add Export component

---
+++ resources/js/Pages/Settings/Export.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3 class="mt-4 text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">Exportação (Em construção)</h3>
+        <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Exporte seus dados para planilhas.</p>
+    </div>
+</template>
