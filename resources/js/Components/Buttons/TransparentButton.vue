c036a6a26cea75ce1a8cc7ec55a8dbcc669677bb
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-03 20:50:21 -0300

    feat: add TransparentButton

---
+++ resources/js/Components/Buttons/TransparentButton.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button class="inline-flex items-center rounded-lg p-2 text-gray-500 hover:text-gray-800 focus:outline-none dark:text-gray-400 dark:hover:text-gray-100">
+        <slot />
+    </button>
+</template>
