6f23c405cc838a3a6e7094038a3a729273586d47
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-03 20:50:38 -0300

    feat: add DotsIcon

---
+++ resources/js/Components/Icons/DotsIcon.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+    </svg>
+</template>
