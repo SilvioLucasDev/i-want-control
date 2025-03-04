672985215c7e37f6b58794c9b796191be4cc56e9
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-04 13:09:02 -0300

    feat: add AddWithdrawalsFromInvestmentsModal

---
+++ resources/js/Pages/Expense/Entries/AddWithdrawalsFromInvestments.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import PrimaryButton from '@/Components/Buttons/PrimaryButton.vue';
+import SecondaryButton from '@/Components/Buttons/SecondaryButton.vue';
+import InputError from '@/Components/Inputs/InputError.vue';
+import InputLabel from '@/Components/Inputs/InputLabel.vue';
+import Select from '@/Components/Inputs/Select.vue';
+import TextInput from '@/Components/Inputs/TextInput.vue';
+import Modal from '@/Components/Modal/Modal.vue';
+
+import { useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+defineProps({
+    showAddWithdrawalsFromInvestmentsModal: Boolean,
+});
+
+const emit = defineEmits(['close']);
+
+const close = (): void => {
+    emit('close');
+};
+
+const form = useForm({
+    type: '',
+    value: '',
+});
+
+const save = (): void => {
+    console.log('DASHBOARD :: Save Withdrawals from Investments', form);
+};
+
+const projectTypeMock = ref<string[]>(['FII', 'Criptomoeda', 'Renda Fixa', 'Tesouro']);
+</script>
+
+<template>
+    <Modal :show="showAddWithdrawalsFromInvestmentsModal" @close="close" max-width="md">
+        <div class="p-6">
+            <h3 class="mb-4 text-lg font-semibold text-gray-900 dark:text-white">Adicionar novo saque de investimento?</h3>
+
+            <form @submit.prevent="save" class="grid gap-4">
+                <div>
+                    <Select id="type" v-model="form.type" label="Tipo" :options="projectTypeMock.map((type, index) => ({ value: index, label: type }))" />
+                    <InputError class="mt-2" :message="form.errors.type" />
+                </div>
+
+                <div>
+                    <InputLabel for="value" value="Valor" />
+                    <TextInput id="value" v-model="form.value" type="text" class="mt-1 block w-full" />
+                    <InputError class="mt-2" :message="form.errors.value" />
+                </div>
+
+                <div class="grid grid-cols-2 gap-2">
+                    <PrimaryButton class="mb-2 me-2 px-5" :class="{ 'opacity-25': form.processing }" :disabled="form.processing"> Salvar </PrimaryButton>
+
+                    <SecondaryButton @click="close"> Cancelar </SecondaryButton>
+                </div>
+            </form>
+        </div>
+    </Modal>
+</template>
