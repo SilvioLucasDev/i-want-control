7a271d6f6771995993beadff79c33d4ef0adeef0
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-04 12:02:25 -0300

    feat: add DashboardCard component

---
+++ resources/js/Components/DashboardCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+    title: String,
+    text: String,
+});
+</script>
+
+<template>
+    <div class="flex items-center justify-between space-x-3 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+        <div>
+            <h5 class="text-sm font-bold text-gray-900 dark:text-white">{{ title }}</h5>
+            <p class="text-lg text-gray-400">{{ text }}</p>
+        </div>
+
+        <slot />
+    </div>
+</template>
