723e093e856d47ec6d613c16f0e3e44ee221ab6a
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-06 00:25:50 -0300

    chore: run lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-        './node_modules/flowbite/**/*.js',
-    ],
+    content: ['./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', './storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue', './node_modules/flowbite/**/*.js'],
 
     theme: {
         extend: {
