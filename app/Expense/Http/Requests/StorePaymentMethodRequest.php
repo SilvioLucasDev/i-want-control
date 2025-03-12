8b2c4918a4cf676beb0e35a2305f468a87eb1025
José Divino <josedivinooficial02@gmail.com>
2025-03-12 20:01:26 -0300

    feat: adds form request store paymentMethod, investment and project

---
+++ app/Expense/Http/Requests/StorePaymentMethodRequest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace App\Expense\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentMethodRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => [
+                'required',
+                'string',
+                'max:255',
+                'unique:payment_methods,name,NULL,NULL,user_id,' . auth()->id(),
+            ],
+        ];
+    }
+}
---
+++ app/Expense/Http/Requests/StoreProjectRequest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace App\Expense\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => [
+                'required',
+                'string',
+                'max:255',
+                'unique:projects,name,NULL,NULL,user_id,' . auth()->id(),
+            ],
+            'hourlyRate' => [
+                'nullable',
+                'numeric',
+            ],
+        ];
+    }
+}
