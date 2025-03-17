74ba4c297c1eaae64ba7474fd1622ae509253a27
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-17 01:16:48 -0300

    refactor: run rector

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
             ProjectSeeder::class,
         ]);
 
-        User::factory(10)->create()->each(function ($user) {
+        User::factory(10)->create()->each(function ($user): void {
             Project::factory(2)->create(['user_id' => $user->id]);
             Investment::factory(2)->create(['user_id' => $user->id]);
             PaymentMethod::factory(2)->create(['user_id' => $user->id]);
