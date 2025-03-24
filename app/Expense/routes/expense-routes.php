f34e738ded9484914bebccb2b6e8790a848f3922
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-03-23 22:50:34 -0300

    refactor: add ziggy in ignore and adjust name of routes

--- app/Expense/routes/expense-routes.php
+++ app/Expense/routes/expense-routes.php
@@ -10,25 +10,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function (): void {
     Route::group(['prefix' => 'expenses'], function (): void {
-        Route::get('/', [ExpenseController::class, 'index'])->name('expenses');
+        Route::get('/', [ExpenseController::class, 'index'])->name('expenses.index');
     });
 
     Route::group(['prefix' => 'payment-methods'], function (): void {
-        Route::get('/', [PaymentMethodController::class, 'index'])->name('payment-methods');
+        Route::get('/', [PaymentMethodController::class, 'index'])->name('payment-methods.index');
         Route::post('/', [PaymentMethodController::class, 'store'])->name('payment-methods.store');
         Route::put('/{paymentMethod}', [PaymentMethodController::class, 'update'])->name('payment-methods.update');
         Route::delete('/{paymentMethod}', [PaymentMethodController::class, 'destroy'])->name('payment-methods.destroy');
     });
 
     Route::group(['prefix' => 'investments'], function (): void {
-        Route::get('/', [InvestmentController::class, 'index'])->name('investments');
+        Route::get('/', [InvestmentController::class, 'index'])->name('investments.index');
         Route::post('/', [InvestmentController::class, 'store'])->name('investments.store');
         Route::put('/{investment}', [InvestmentController::class, 'update'])->name('investments.update');
         Route::delete('/{investment}', [InvestmentController::class, 'destroy'])->name('investments.destroy');
     });
 
     Route::group(['prefix' => 'projects'], function (): void {
-        Route::get('/', [ProjectController::class, 'index'])->name('projects');
+        Route::get('/', [ProjectController::class, 'index'])->name('projects.index');
         Route::post('/', [ProjectController::class, 'store'])->name('projects.store');
         Route::put('/{project}', [ProjectController::class, 'update'])->name('projects.update');
         Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
