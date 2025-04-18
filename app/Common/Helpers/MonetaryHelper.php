06b777ad99d809a1c53689f6ece7c1f277a2e362
SilvioLucasDev <silviolucas_santos@hotmail.com>
2025-04-18 01:36:10 -0300

    feat: add DateTimeHelper and MonetaryHelper

--- app/Common/Helpers/MonetaryHelper.php
+++ app/Common/Helpers/MonetaryHelper.php
@@ -29,3 +29,15 @@ if (!function_exists('convert_to_decimal')) {
         return (string) ($value / 100);
     }
 }
+
+if (!function_exists('calculate_total_receivable')) {
+    /**
+     * Calculate the total receivable based on the hourly rate and duration.
+     */
+    function calculate_total_receivable(int $hourlyRate, string $duration): int
+    {
+        $totalSeconds = get_time_in_seconds($duration);
+
+        return (int) (($totalSeconds / 3600) * $hourlyRate);
+    }
+}
